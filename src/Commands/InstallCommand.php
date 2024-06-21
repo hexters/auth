36a499f39c35795167638c5a7cdfa5f30b3daf24
Asep <hexters@gmail.com>
2024-06-21 22:26:19 +0700

    mary ui reseted

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -28,11 +28,30 @@ class InstallCommand extends Command
         $this->call('livewire:publish', [
             '--config'
         ]);
-
-        $this->call('livewire:layout');
-        $this->call('vendor:publish', [ '--tag' => 'mary.config' ]);
+        
+        $this->call('vendor:publish', ['--tag' => 'mary.config']);
         $this->call('volt:install');
         $this->call('mary:install');
         $this->call('view:clear');
+        $this->call('livewire:layout', ['--force' => true]);
+
+        $this->reset();
+    }
+
+    protected function reset()
+    {
+        // Reset route web
+        $stub = file_get_contents(__DIR__ . '/stubs/web.stub');
+        file_put_contents(base_path('routes/web.php'), $stub);
+
+        // Reset Livewire welcome
+        if (file_exists($welcome = app_path('Livewire/Welcome.php'))) {
+            @unlink($welcome);
+        }
+
+        // Reset welcome blade
+        if (file_exists($welcome = resource_path('views/livewire/welcome.blade.php'))) {
+            @unlink($welcome);
+        }
     }
 }
